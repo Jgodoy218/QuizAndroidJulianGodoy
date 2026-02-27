a15d968e0c11bb13aeb26cc6ddb96ae3ee2a81bf
jgodoy218 <jgodoy218@unab.edu.co>
2026-02-27 09:34:59 -0500

    feat:Primera pantalla

--- app/src/main/java/me/juliangodoy/quizandroidjuliangodoy/MainActivity.kt
+++ app/src/main/java/me/juliangodoy/quizandroidjuliangodoy/MainActivity.kt
@@ -4,13 +4,20 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import me.juliangodoy.quizandroidjuliangodoy.ui.theme.QuizAndroidJulianGodoyTheme
 
 class MainActivity : ComponentActivity() {
@@ -18,30 +25,28 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            QuizAndroidJulianGodoyTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    QuizAndroidJulianGodoyTheme {
-        Greeting("Android")
+fun PrimeraPantalla() {
+    Row(modifier = Modifier
+        .background(Color.Green)
+
+    ) {
+        Column(modifier = Modifier
+                .background(Color.Yellow).
+                 fillMaxSize()
+        ){ Text(text = "Item 1",
+                    modifier = Modifier
+                        .background(Color.Yellow).
+                        fillMaxSize()
+                        .padding(10.dp)
+
+        )
+                }
     }
 }
